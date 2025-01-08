98d0f1522c17b5cb9cc5e3fd7d768e67c79d739c
j-wye <4mimprove@gmail.com>
2025-01-08 17:27:01 +0900

    0108

--- whole_settings.sh
+++ whole_settings.sh
@@ -9,3 +9,18 @@ NUM_THREADS=$(lscpu | grep '^CPU(s):' | awk '{print $2}')
 echo "alias cb='colcon build --parallel-workers $NUM_THREADS --cmake-args -DCMAKE_BUILD_TYPE=Release'" >> ~/.bashrc
 source ~/.bashrc && cb
 source install/setup.bash
+
+# Install RTAB-Map
+cd ~/hc_ws/src && mkdir RTAB_Map && cd RTAB_Map
+git clone https://github.com/introlab/rtabmap.git -b humble-devel
+git clone https://github.com/introlab/rtabmap_ros.git -b ros2
+cd ~/hc_ws
+rosdep update && rosdep install --from-paths src --ignore-src -r -y
+sb
+
+echo "export RCUTILS_LOGGING_USE_STDOUT=1" >> ~/.bashrc
+echo "export RCUTILS_LOGGING_BUFFERED_STREAM=1" >> ~/.bashrc
+# Optional, but if you like colored logs:
+echo "export RCUTILS_COLORIZED_OUTPUT=1" >> ~/.bashrc
+# Recommend to use cyclonedds than fastrtps
+export RMW_IMPLEMENTATION=rmw_cyclonedds_cpp
